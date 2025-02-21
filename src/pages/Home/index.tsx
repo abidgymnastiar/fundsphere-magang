1bf206d04f852e4dc7cb054607166d267e82fbdb
abidgymnastiar <abidgymnastiar@gmail.com>
2025-02-21 08:31:39 +0700

    refactor: ganti nama di folder pages

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,9 @@
-import Features from "./featuress";
-import Hero from "./heroo";
+import Features from "./Features";
+import Hero from "./hero";
 import Insight from "./Insight";
-import News from "./newss";
+import News from "./News";
 import Pricing from "./Pricing";
-import Testimony from "./testimonii";
+import Testimony from "./Testimoni";
 
 function Home() {
   return (
